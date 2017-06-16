bba07c496f22644f7b3c759353e2ed74cc575722
dschien <dschien@gmail.com>
2017-06-16 10:46:06 +0100

    increased version number

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = pypandoc.convert('README.md', 'rst')
 
 setup(
     name='excel-modelling-helper',
-    version='0.3.3',
+    version='0.4.0',
     description='Use Excel to define your model parameters.',
     long_description=(long_description + '\n\n' +
                       read('CHANGES.rst')),
